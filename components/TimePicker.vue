850faab48dcdbdb6a0c981e9a510dd2f99755d28
LarryGF <larrywtf609@gmail.com>
2019-11-26 17:06:22 -0500

    Rubbish

---
+++ components/TimePicker.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="290" class="text-xs-center">
+    <v-time-picker v-model="picker">
+      <v-btn class="primary" flat @click="save">Accept</v-btn>
+      <v-btn class="error" flat @click="$emit('close')">Cancel</v-btn>
+    </v-time-picker>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "TimePickerDiag",
+  props: {
+    dialog: Boolean,
+    selectedOrder: Object
+  },
+  data() {
+    return {
+      picker: null
+    };
+  },
+  methods: {
+    save:function(){
+      var data = {order:this.selectedOrder, picker:this.picker}
+      this.$emit('save',data)
+    }
+  }
+};
+</script>
--- pages/main.vue
+++ pages/main.vue
@@ -1,48 +1,46 @@
 
 <template>
   <v-layout column justify-center align-center>
+            <TimePickerDiag :dialog="dialog" :selectedOrder="selectedOrder" @close="dialog=false" @save="setTime"/>
     <v-flex xs12 sm8 md6>
+      <v-btn color="primary" right bottom fixed icon fab @click="addOrder">
+        <v-icon>add</v-icon>
+      </v-btn>
       <div class="text-xs-center">
         <v-avatar class="my-4" size="150">
-
-        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+          <img src="/v.png" alt="Vuetify.js" class="mb-5">
         </v-avatar>
       </div>
-      <v-card  width="1000">
+      <v-card width="1000">
         <v-toolbar dark>
-
-        <v-card-title class="headline">Define the orders for each group</v-card-title>
+          <v-card-title class="headline">Define the orders for each group</v-card-title>
+          <v-spacer></v-spacer>
         </v-toolbar>
         <v-card-text>
-         <template>
-           <v-layout row justify-space-around>
-             <v-avatar color="blue" class="mt-2 mx-3">
-               <h3 style="color:white">1</h3>
-             </v-avatar>
+          <v-layout column v-for="order in orders" :key="order.id">
+            
+            <v-layout row justify-space-around class="mt-3">
+              <v-avatar color="blue" class="mt-2 mx-3">
+                <h3 style="color:white">{{order.id}}</h3>
+              </v-avatar>
 
-             <v-autocomplete outline 
-             dense
-             label="Groups" class="mx-1"></v-autocomplete>
-             <v-flex xs2>
-
-             <v-autocomplete outline
-             dense
-             label="Target" class="mx-1"></v-autocomplete>
-             </v-flex>
+              <v-autocomplete outline v-model="order.group" dense label="Groups" class="mx-1" ></v-autocomplete>
+              <v-flex xs2>
+                <v-autocomplete outline v-model="order.target" dense label="Target" class="mx-1"></v-autocomplete>
+              </v-flex>
               <v-flex xs3>
-
-             <v-text-field outline
-             label="Additional text" class="mx-1"></v-text-field>
-             </v-flex>
-             <v-btn icon color="blue" large class="mt-2"><v-icon color="white">alarm_add</v-icon></v-btn>
-             <blockquote class="mt-3 mx-2">time</blockquote>
-           </v-layout>
-           <v-divider class="my-2"></v-divider>
-         </template>
+                <v-text-field outline label="Additional text" v-model="order.text" class="mx-1"></v-text-field>
+              </v-flex>
+              <v-btn icon color="blue" large class="mt-2">
+                <v-icon color="white" @click.stop="setDialog()">alarm_add</v-icon>
+              </v-btn>
+              <blockquote class="mt-4 mx-2">{{order.time}}</blockquote>
+            </v-layout>
+            <v-divider class="mb-2"></v-divider>
+          </v-layout>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -50,11 +48,54 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-       
-      }
+import TimePickerDiag from "../components/TimePicker.vue";
+export default {
+  data() {
+    return {
+      count: 0,
+      picker:null,
+      dialog: false,
+      orders: [
+        {
+          id: 0,
+          groups: [],
+          target: "",
+          text: "",
+          time: "00:00"
+        }
+      ],
+      selectedOrder:null
+    };
+  },
+  components: {
+    TimePickerDiag
+  },
+  methods: {
+    addOrder: function() {
+      this.count++;
+      console.log(this.count);
+      this.orders.push({
+        id: this.count,
+        groups: [],
+        target: "",
+        text: "",
+        time: "00:00"
+      });
+    },
+    setTime: function(data) {
+      console.log(data);
+      this.dialog = false
+      var index = this.orders.indexOf(data.order)
+      this.orders[index].time = data.picker
+      // this.order.time = data
+      // this.dialog = false
+    },
+    setDialog: function(order){
+      console.log('er')
+      this.selectedOrder=order
+      this.dialog = true
+
     }
   }
+};
 </script>
